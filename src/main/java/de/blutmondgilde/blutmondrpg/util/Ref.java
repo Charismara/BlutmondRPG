54cd698c0b7d2bd52aebc57be2597becd2f4e0bf
Christian Raoulis <craoulis+justchunks@gmail.com>
2020-04-16 11:07:44 +0200

    Initial commit

---
+++ src/main/java/de/blutmondgilde/blutmondrpg/util/Ref.java
@@ -0,0 +1,10 @@
+package de.blutmondgilde.blutmondrpg.util;
+
+import de.blutmondgilde.blutmondrpg.BlutmondRPG;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public final class Ref {
+    public static final String MOD_ID = "blutmondrpg";
+    public static final Logger LOGGER = LogManager.getLogger(BlutmondRPG.class);
+}
